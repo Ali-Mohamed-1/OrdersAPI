d6a83c782bdd16a83124a5cfb5a0cd83fc097877
Ali Mohamed <ali.elsabbagh@ejust.edu.eg>
2026-07-17 01:37:19 +0300

    Add simple POST endpoint

---
+++ OrdersAPI/Models/CreateOrderRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace OrdersAPI.Models
+{
+    public class CreateOrderRequest
+    {
+        public List<OrderItem> orderItems { get; set; } = new List<OrderItem>();
+    }
+}
---
+++ OrdersAPI/Models/OrderItem.cs
@@ -0,0 +1,8 @@
+﻿namespace OrdersAPI.Models
+{
+    public class OrderItem
+    {
+        public int id { get; set; }
+        public int quantity { get; set; }
+    }
+}
