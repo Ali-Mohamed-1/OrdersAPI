19d65476a9770f104731230b0cf40ee1d3cfdaed
Ali Mohamed <ali.elsabbagh@ejust.edu.eg>
2026-07-21 20:57:15 +0300

    Add built-in automatic validations

--- OrdersAPI/Program.cs
+++ OrdersAPI/Program.cs
@@ -1,15 +1,39 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using OrdersAPI.Filters;
 using OrdersAPI.Middlewares;
 using OrdersAPI.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers(options =>
 {
-    options.Filters.Add<ValidateModelFilter>();
+    // APPROACH 1: Custom Action Filter
+    // options.Filters.Add<ValidateModelFilter>();
+});
+
+// APPROACH 2: Framework Built-in [ApiController] Automatic Validation
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{    
+    options.InvalidModelStateResponseFactory = actionContext =>
+    {
+        var errors = actionContext.ModelState
+            .Where(e => e.Value?.Errors.Count > 0)
+            .ToDictionary(
+                kvp => kvp.Key,
+                kvp => kvp.Value!.Errors.Select(e => e.ErrorMessage).ToArray()
+            );
+
+        var customProblem = new
+        {
+            Status = 400,
+            Title = "Model Validation Failed",
+            Errors = errors
+        };
+
+        return new BadRequestObjectResult(customProblem);
+    };
 });
 
 builder.Services.AddFluentValidationAutoValidation();
